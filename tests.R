f59d369673d0c4abfec50438c5bc45e4096575f2
michbur <michalburdukiewicz@gmail.com>
2022-12-30 12:11:57 +0100

    added test for scaling

--- tests.R
+++ tests.R
@@ -2,14 +2,18 @@ library(testthat)
 
 source("functions/generate-data.R")
 source("functions/imputing.R")
+source("functions/scaling.R")
 
 set.seed(1)
 
-df <- create_df(n_metabolites = 50, n_samples = 20, frac_na = 0.05)
+mdf <- create_df(n_metabolites = 20, n_samples = 20, frac_na = 0.05)
 
-expect_true(any(is.na(all_safe_imp_funs[["safe_impute_mice_norm.boot"]](df))))
+expect_true(any(is.na(all_safe_imp_funs[["safe_impute_mice_norm.boot"]](mdf))))
 
-expect_false(any(is.na(all_safe_imp_funs[["safe_impute_tknn"]](df))))
+expect_false(any(is.na(all_safe_imp_funs[["safe_impute_tknn"]](mdf))))
 
 #all_safe_imp_funs[["safe_impute_gsimp"]](df)
 #impute_gsimp(df)
+
+scaled <- scale_df(mdf)
+expect_equal(unscale_df(scaled), mdf)
