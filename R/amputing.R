77681683cb21e51160b98f26927609b9c2abfcde
KrystynaGrzesiak <krygrz11@gmail.com>
2023-04-06 17:17:15 +0200

    adds amputation

--- R/amputing.R
+++ R/amputing.R
@@ -25,7 +25,7 @@
 #'
 
 insert_MCAR <- function(dat, ratio = 0) {
-  mice::ampute(data = dat, prop = ratio, mech = "MCAR")$amp
+  mice::ampute(data = dat, prop = ratio, mech = "MCAR", bycases = FALSE)$amp
 }
 
 ######## MAR
@@ -49,7 +49,7 @@ insert_MCAR <- function(dat, ratio = 0) {
 #'
 
 insert_MAR <- function(dat, ratio = 0) {
-  mice::ampute(data = dat, prop = ratio, mech = "MAR")$amp
+  mice::ampute(data = dat, prop = ratio, mech = "MAR", bycases = FALSE)$amp
 }
 
 
@@ -72,13 +72,12 @@ insert_MAR <- function(dat, ratio = 0) {
 #' @examples
 #' m <- matrix(rnorm(10), 5, 2)
 #' insert_MNAR(m, ratio = 0.3)
-#'
 #' @export
 #'
 
-insert_MNAR <- function(dat, ratio = 0) {
-  n <- ncol(x)
-  sum_value <- n * p
+insert_MNAR <- function(dat, ratio = 0.1) {
+  n <- ncol(dat)
+  sum_value <- n * ratio
   ratio_cols <- runif(n - 1, 0, 0.9)
   p_vec <- diff(sort(c(ratio_cols, 0, 1))) * sum_value
 
@@ -89,8 +88,8 @@ insert_MNAR <- function(dat, ratio = 0) {
   }
 
   do.call(cbind,
-          lapply(1L:ncol(x), function(ith_col_id) {
-            ith_col <- x[, ith_col_id]
+          lapply(1L:ncol(dat), function(ith_col_id) {
+            ith_col <- dat[, ith_col_id]
             ith_col[ith_col < quantile(ith_col, p_vec[[ith_col_id]])] <- NA
             ith_col
           })
@@ -129,8 +128,8 @@ insert_MNAR <- function(dat, ratio = 0) {
 #' The sum of \code{mcar_ratio} + \code{mar_ratio} + \code{mnar_ratio} should
 #' not surpass 1, otherwise the function will throw an error.
 #'
-#' Note that the missing mechanisms are used in the following order: MCAR, MNAR,
-#' MAR. It may happen that some of missing values will overlap themselves and
+#' Note that the missing mechanisms are used in the following order: MAR, MNAR,
+#' MCAR. It may happen that some of missing values will overlap themselves and
 #' in the result missing ratio may be slightly smaller.
 #'
 #' @returns A \code{matrix} with NA values inserted.
@@ -140,28 +139,36 @@ insert_MNAR <- function(dat, ratio = 0) {
 #' \code{\link[imputomics]{insert_MNAR}}
 #'
 #' @examples
-#' m <- matrix(rnorm(10), 5, 2)
-#' simulate_miss_value(m, mcar_ratio = 0.1, mar_ratio = 0.05, mnar_ratio = 0.15)
+#' m <- as.data.frame(matrix(rnorm(10), 5, 2))
+#' simulate_miss_value(m, mcar = 0.1, mar = 0.05, mnar = 0.15)
 #'
 #' @export
 #'
 
 simulate_miss_value <- function(data_set,
-                                mcar_ratio = 0,
-                                mar_ratio = 0,
-                                mnar_ratio = 0){
-  if(!("matrix" %in% class(data_set)))
-    stop("Variable data should be a matrix.") # as.matrix()
+                                mcar = 0,
+                                mar = 0,
+                                mnar = 0){
+  if(!("data.frame" %in% class(data_set)))
+    stop("Variable data should be a data.frame") # as.matrix()
 
-  if ((mcar + mar + mnar > 1) || any(c(mcar_ratio, mar_ratio, mnar_ratio) < 0))
+  if ((mcar + mar + mnar > 1) || any(c(mcar, mar, mnar) < 0))
     stop("Sum of mcar, mar and mnar should be between 0 and 1.")
 
-  if(mcar > 0)
-    data_set <- insert_MCAR(data_set, mcar_ratio)
-  if(mnar > 0)
-    data_set <- insert_MNAR(data_set, mnar_ratio)
+  amputed_mar <- amputed_mnar <- amputed_mcar <- data_set
+
   if(mar > 0)
-    data_set <- insert_MAR(data_set, mar_ratio)
+    amputed_mar <- insert_MAR(data_set, mar)
+  if(mnar > 0)
+    amputed_mnar <- insert_MNAR(data_set, mnar)
+  if(mcar > 0)
+    amputed_mcar <- insert_MCAR(data_set, mcar)
+
+  data_set[is.na(amputed_mar)] <- NA
+  data_set[is.na(amputed_mnar)] <- NA
+  data_set[is.na(amputed_mcar)] <- NA
 
   data_set
 }
+
+
