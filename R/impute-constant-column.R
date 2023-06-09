7d0b55d611ea4d470374c3564d8d31607b41b594
michbur <michalburdukiewicz@gmail.com>
2023-06-09 13:32:24 +0200

    re-organizing impute functions

---
+++ R/impute-constant-column.R
@@ -0,0 +1,171 @@
+#' Helper function.
+#' Imputes with a constant value
+#'
+#' @inheritParams impute_zero
+#' @noRd
+#' @keywords internal
+
+impute_constant <- function(missdf, constant_value) {
+  
+  missdf[is.na(missdf)] <- constant_value
+  missdf
+}
+
+#' Helper function.
+#' Basic imputation of missing values into dataframe.
+#' Zero, minimum, half-minimum, mean, median and random.
+#'
+#' @inheritParams impute_zero
+#'
+#' @param compute_values one of the basic imputation methods
+#' @noRd
+#' @keywords internal
+
+impute_per_column <- function(missdf, compute_values) {
+  
+  check_missdf(missdf)
+  
+  imputed_values_vector <- compute_values(missdf)
+  
+  do.call(cbind, lapply(1L:ncol(missdf), function(ith_column_id) {
+    impute_constant(missdf[ith_column_id],
+                    imputed_values_vector[[ith_column_id]])
+  }))
+}
+
+#' \strong{zero} imputation.
+#'
+#' A function to replace \code{NA} in the data frame by  zeros (0).
+#'
+#' @param missdf a data frame with missing values to be imputed
+#' containing features in columns and samples in rows.
+#'
+#' @returns A \code{data.frame} with imputed values by \strong{zero} method.
+#'
+#' @examples
+#' data(sim_miss)
+#' impute_zero(sim_miss)
+#'
+#' @keywords constant
+#'
+#' @export
+impute_zero <- function(missdf) {
+  
+  check_missdf(missdf)
+  
+  impute_constant(missdf, constant_value = 0)
+}
+  
+
+
+#' Helper function. Minimum imputation.
+#'
+#' @inheritParams compute_col_random
+#' @noRd
+#' @keywords internal
+compute_col_min <- function(x)
+  lapply(x, min, na.rm = TRUE)
+
+#' Helper function. Mean imputation.
+#' @inheritParams compute_col_random
+#' @noRd
+compute_col_mean <- function(x)
+  lapply(x, mean, na.rm = TRUE)
+
+#' Helper function. Half-minimum imputation.
+#' @noRd
+#' @inheritParams compute_col_random
+#'
+#' @keywords internal
+compute_col_halfmin <- function(x)
+  lapply(x, function(i) min(i, na.rm = TRUE)/2)
+
+#' Helper function. Median imputation.
+#' @noRd
+#' @inheritParams compute_col_random
+#'
+#' @keywords internal
+compute_col_median <- function(x)
+  lapply(x, mean, na.rm = TRUE)
+
+
+#' \strong{minimum} imputation.
+#'
+#' A function to replace \code{NA} in the data frame by minimum values.
+#'
+#' @inheritParams impute_zero
+#'
+#' @returns A \code{data.frame} with imputed values by \strong{minimum} method.
+#'
+#' @examples
+#' data(sim_miss)
+#' impute_min(sim_miss)
+#'
+#' @keywords constant
+#'
+#' @export
+impute_min <- function(missdf)
+  impute_per_column(missdf, compute_col_min)
+
+
+#' \strong{mean} imputation.
+#'
+#' A function to replace \code{NA} in the data frame by  mean values.
+#'
+#' @inheritParams impute_zero
+#'
+#' @returns A \code{data.frame} with imputed values by \strong{mean} method.
+#'
+#' @examples
+#' data(sim_miss)
+#' impute_mean(sim_miss)
+#'
+#' @keywords constant
+#'
+#' @export
+impute_mean <- function(missdf) {
+  missdf <- data.frame(missdf)
+  impute_per_column(missdf, compute_col_mean)
+  
+}
+
+
+#' \strong{half-minimum} imputation.
+#'
+#' A function to replace \code{NA} in the data frame by the half minimum values.
+#'
+#' @inheritParams impute_zero
+#'
+#' @returns A \code{data.frame} with imputed values by \strong{half-minimum}
+#' method.
+#'
+#' @examples
+#' data(sim_miss)
+#' impute_halfmin(sim_miss)
+#'
+#' @keywords constant
+#'
+#' @export
+impute_halfmin <- function(missdf) {
+  missdf <- data.frame(missdf)
+  impute_per_column(missdf, compute_col_halfmin)
+}
+
+
+#' \strong{median} imputation.
+#'
+#' A function to replace \code{NA} in the data frame by a median values.
+#'
+#' @inheritParams impute_zero
+#'
+#' @returns A \code{data.frame} with imputed values by \strong{median} method.
+#'
+#' @examples
+#' data(sim_miss)
+#' impute_median(sim_miss)
+#'
+#' @keywords constant
+#'
+#' @export
+impute_median <- function(missdf)
+  impute_per_column(missdf, compute_col_median)
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,30 @@
+#' Convert an imputing function into its safe version.
+#'
+#' @inheritParams impute_zero
+#'
+#' @param imputing_function a function (imputation method) that takes
+#' missdf as an input
+#'
+#' @return A \code{data.frame} with imputed values or the
+#' \strong{missdf} if the imputing function failed to converge.
+#'
+#' @keywords internal
+
+safe_impute <- function(imputing_function, missdf) {
+  imputed <- structure(structure(list(), class = "try-error"))
+  n <- 1
+  while(inherits(imputed, "try-error") & n < 101) {
+    imputed <- try(imputing_function(missdf), silent = TRUE)
+    n <- n + 1
+  }
+  
+  if(inherits(imputed, "try-error")) {
+    missdf
+  } else {
+    imputed
+  }
+}
+
 #' Checks (asserts) if an object is a data.frame
 #'
 #' @param missdf an object.
@@ -10,6 +37,14 @@ check_missdf <- function(missdf, above_zero = FALSE) {
   if(!testDataFrame(missdf))
     stop("'missdf' must be a data.frame or tibble.")
   
+  if(all(sapply(missdf, function(i) !is.na(i))))
+    if(any(sapply(missdf, checkmate::testNumeric, lower = 0, upper = 0))) {
+      warning("No NAs identified, but the data contains zeros.\nMake sure that you are marking missing values with 0.")
+    } else {
+      message("No NAs identified.")
+    }
+      
+  
   if(above_zero) {
     if(!all(sapply(missdf, testNumeric, lower = 0)))
       stop("'missdf' must contain only numeric data above 0.")
@@ -17,5 +52,7 @@ check_missdf <- function(missdf, above_zero = FALSE) {
     if(!all(sapply(missdf, testNumeric)))
       stop("'missdf' must contain only numeric data.")
   }
-    
+  
 }
+
+# add a function for checking the output
