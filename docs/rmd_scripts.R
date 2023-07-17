401821eb7d35df34362292594f519f957fabdeb8
jarochi <jaroslaw.chilimoniuk@gmail.com>
2023-07-17 12:30:19 +0200

    readme.rmd

---
+++ docs/rmd_scripts.R
@@ -0,0 +1,33 @@
+imputomics_citation <- function() {
+  "
+
+## Citation
+
+Jarosław Chilimoniuk, Krystyna Grzesiak, Jakub Kała, Dominik Nowakowski,
+Małgorzata Bogdan, Michał Ciborowski, Adam Krętowski, Michał Burdukiewicz.
+Imputomics: comprehensive missing data imputation for metabolomics data.
+[link](link).
+
+  "
+}
+
+imputomics_links <- function() {
+  "
+
+## Links to implemented packages?
+
+
+
+  "
+}
+
+imputomics_contact <- function() {
+  "
+
+## Contact
+
+If you have any questions, suggestions or comments, contact [Michal Burdukiewicz](mailto:michalburdukiewicz@gmail.com).
+
+
+  "
+}
