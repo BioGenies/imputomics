750af64e23f3dba0db8e2bdfcbe38eb0b7cee7f0
michbur <michalburdukiewicz@gmail.com>
2023-07-23 16:25:20 +0200

    added funding

--- README.md
+++ README.md
@@ -65,7 +65,7 @@ imputomics::imputomics_gui()
 
 <!-- # How to cite -->
 
-# How to cite?
+## How to cite?
 
 Jarosław Chilimoniuk, Krystyna Grzesiak, Jakub Kała, Dominik Nowakowski,
 Małgorzata Bogdan, Michał Ciborowski, Adam Krętowski, Michał
@@ -76,3 +76,15 @@ for metabolomics data (submitted).
 
 If you have any questions, suggestions or comments, contact [Michal
 Burdukiewicz](mailto:michalburdukiewicz@gmail.com).
+
+## Funding and acknowledgements
+
+We want to thank the Clinical Research Centre (Medical University of
+Białystok) members for fruitful discussions. K.G. wants to acknowledge
+grant no. 2021/43/O/ST6/02805 (National Science Centre). M.C.
+acknowledges grant no. B.SUB.23.533 (Medical University of Białystok).
+The study was supported by the Ministry of Education and Science funds
+within the project ‘Excellence Initiative - Research University’. We
+also acknowledge the Center for Artificial Intelligence at the Medical
+University of Białystok (funded by the Ministry of Health of the
+Republic of Poland).
--- inst/readme_scripts.R
+++ inst/readme_scripts.R
@@ -1,7 +1,7 @@
 imputomics_citation <- function() {
   "
 
-# How to cite?
+## How to cite?
 
 Jarosław Chilimoniuk, Krystyna Grzesiak, Jakub Kała, Dominik Nowakowski, Małgorzata Bogdan, Michał Ciborowski, Adam Krętowski, Michał Burdukiewicz (2023).
 Imputomics: comprehensive missing data imputation for metabolomics data (submitted).
@@ -12,7 +12,7 @@ Imputomics: comprehensive missing data imputation for metabolomics data (submitt
 imputomics_links <- function() {
   "
 
-# Links to implemented packages?
+## Links to implemented packages?
 
 
 
@@ -26,6 +26,17 @@ imputomics_contact <- function() {
 
 If you have any questions, suggestions or comments, contact [Michal Burdukiewicz](mailto:michalburdukiewicz@gmail.com).
 
+
+  "
+}
+
+
+imputomics_funding <- function() {
+  "
+
+## Funding and acknowledgements
+
+We want to thank the Clinical Research Centre (Medical University of Białystok) members for fruitful discussions. K.G. wants to acknowledge grant no. 2021/43/O/ST6/02805 (National Science Centre). M.C. acknowledges grant no. B.SUB.23.533 (Medical University of Białystok). The study was supported by the Ministry of Education and Science funds within the project 'Excellence Initiative - Research University'. We also acknowledge the Center for Artificial Intelligence at the Medical University of Białystok (funded by the Ministry of Health of the Republic of Poland).
 
   "
 }
