9813b9c052e2c84fb77015a61abe017c9d9b3bf7
jarochi <jaroslaw.chilimoniuk@gmail.com>
2023-07-26 10:28:51 +0200

    some fixes

--- R/imputing.R
+++ R/imputing.R
@@ -340,7 +340,7 @@ impute_gsimp <- function(missdf,
   imputed[["data_imp"]]
 }
 
-#' \strong{kNN} imputation.
+#' \strong{VIM kNN} imputation.
 #'
 #' K Nearest Neighbors. A function to replace \code{NA} in the data frame by
 #' [VIM::kNN()].
@@ -445,7 +445,7 @@ impute_regimpute <- function(missdf, verbose = FALSE, fillmethod = "row_mean",
 }
 
 
-#' \strong{Singular Value Decomposition - SVD} imputation.
+#' \strong{bcv SVD} imputation.
 #'
 #' A function to replace \code{NA} in the data frame by
 #' [bcv::impute.svd()].
@@ -473,7 +473,7 @@ impute_bcv_svd <- function(missdf, ...) {
 }
 
 
-#' \strong{kNN} imputation.
+#' \strong{imputation kNN} imputation.
 #'
 #' A function to replace \code{NA} in the data frame by
 #' [imputation::kNNImpute()].
@@ -594,10 +594,10 @@ impute_mice_mixed <- function(missdf) {
 }
 
 
-#' \strong{Metabolomic Non-negative Matrix Factorization - mNMF} imputation.
+#' \strong{mNMF} imputation.
 #'
 #' A function to replace \code{NA} in the data frame using
-#' non-negative Matrix Factorization.
+#' metabolomic Non-negative Matrix Factorization.
 #'
 #' @importFrom NMF nmf.getOption
 #'
